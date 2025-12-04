fc63ebd909a74d7ce9febe6239af9d679288e86e
Roderick Boekdrukker <roderick@boekdrukker.com>
2025-12-04 13:09:12 +0100

    Fixing Layout.tsx not to pass lang

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,27 +1,12 @@
 // components/layout/Layout.tsx
 import React, { ReactNode } from "react";
-import Menu from "../Menu";
-import Footer from "../Footer";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return (
-    <div className="min-h-screen flex flex-col bg-slate-900">
-      {/* Top navigation */}
-      <header>
-        <Menu />
-      </header>
-
-      {/* Page content */}
-      <main className="flex-1">
-        {children}
-      </main>
-
-      {/* Global footer */}
-      <Footer />
-    </div>
-  );
+  // Currently just a transparent wrapper.
+  // We're handling Menu + Footer in _app.js.
+  return <>{children}</>;
 }

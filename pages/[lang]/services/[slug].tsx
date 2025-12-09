874ed37a54ace595a68a3dec66ca4482e5e5716d
Roderick Boekdrukker <roderick@boekdrukker.com>
2025-12-09 15:51:03 +0100

    Changing html for services

--- pages/[lang]/services/[slug].tsx
+++ pages/[lang]/services/[slug].tsx
@@ -125,7 +125,7 @@ export default function ServiceDetailPage({ lang }: ServiceDetailPageProps) {
 
           {/* Body (rich text from Directus) */}
             {article.body && (
-              <div
+              <div id="debug_me"
                 className="text-slate-900 text-base leading-relaxed space-y-4"
                 dangerouslySetInnerHTML={{ __html: article.body }}
               />

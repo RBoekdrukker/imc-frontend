2b07d25337104079857812e2be2684905ed5c3fd
Roderick Boekdrukker <roderick@boekdrukker.com>
2025-12-11 14:03:38 +0100

    Adding multilingual homescreen functionality

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,10 +3,12 @@ import React, { ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
+  lang?: string;
 }
 
 export default function Layout({ children }: LayoutProps) {
   // Currently just a transparent wrapper.
   // We're handling Menu + Footer in _app.js.
+  <Menu lang={lang} />
   return <>{children}</>;
 }
